daf2fd6386737f7d8264b73e1854a1e280ce7acd
algogeek <apara20_ug@ece.nits.ac.in>
2022-01-08 09:31:05 +0530

    homepage_added

---
+++ Frontend/ct_frontend/Components/Navbar.js
@@ -0,0 +1,103 @@
+import Link from "next/link";
+import { useState } from "react";
+import Logo from '../public/images/logo_CT.svg'
+import Image from "next/image";
+
+export default function Navbar() {
+
+
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <>
+      
+
+      <nav className="flex items-center flex-wrap bg-bgBlack p-4">
+        <Link href="/">
+          <a className="inline-flex items-center p-2 lg:ml-8">
+            <Image src={Logo}/>
+          </a>
+        </Link>
+
+        <button
+          className="inline-flex p-3 hover:bg-gray-600 rounded md:hidden text-white ml-auto hover:text-white outline-none"
+          onClick={handleClick}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+
+        <div
+          className={`${
+            active ? "" : "hidden"
+          }   w-full md:inline-flex md:flex-grow md:w-auto`}
+         onClick={handleClick}>
+          <div className="md:inline-flex md:flex-row md:ml-16 md:mr-auto md:w-auto w-full md:justify-between md:items-center items-center flex flex-col md:h-auto md:space-x-3">
+            <Link href="/">
+              <a
+                id="navbtn"
+                className="md:inline-flex md:w-auto w-full px-6 py-2 rounded text-white ease-in-out duration-300"
+              >
+                Home
+              </a>
+            </Link>
+            <Link href="/">
+              <a
+                id="navbtn"
+                className="md:inline-flex md:w-auto w-full px-6 py-2 rounded text-white ease-in-out duration-300"
+              >
+                Contests
+              </a>
+            </Link>
+          </div>
+
+{/*          
+            <button
+              id="loginbtn"
+              className="md:inline-flex md:w-auto w-full px-6 items-center justify-center rounded-md py-2 ease-in-out duration-300 md:mr-10 font-semibold"
+             
+            >
+              Log Out
+            </button> */}
+       
+            <>
+              <Link href="/">
+                <button
+                  id="signup"
+                  className="md:inline-flex md:w-auto w-full items-center justify-center rounded px-6 py-2 ease-in-out duration-300 md:mr-4 font-semibold"
+                >
+                  Sign Up
+                </button>
+              </Link>
+
+              <Link href="/">
+                <button
+                  id="loginbtn"
+                  className="md:inline-flex md:w-auto w-full items-center justify-center rounded px-6 py-2 ease-in-out duration-300 md:mr-10 font-semibold"
+                >
+                  Log In
+                </button>
+              </Link>
+            </>
+        
+        </div>
+      </nav>
+    </>
+  );
+}
--- Frontend/ct_frontend/pages/index.js
+++ Frontend/ct_frontend/pages/index.js
@@ -1,10 +1,191 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import Image from "next/image";
+import Link from "next/link";
+// import styles from '../styles/Home.module.css'
+import Headerimg from "../public/images/Saly-10.png";
+import CodeImg from "../public/images/code.png";
+import ArrowRightImg from "../public/images/arrow-right.png";
+import CodeEditorImg from "../public/images/code-editor.png";
+
+import { motion } from 'framer-motion';
+import {BsArrowUpRightCircle} from 'react-icons/bs'
+
+const leftVariants = {
+  hidden: {
+    opacity: 0,
+    y: '-100vh'
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { delay: 0.3, duration: 1.2 }
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: 'easeInOut' }
+  }
+};
+
+const rightVariants = {
+  hidden: {
+    opacity: 0,
+    y: '100vh'
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { delay: 0.3, duration: 1.2 }
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: 'easeInOut' }
+  }
+};
+
 
 export default function Home() {
+
+
+
   return (
     <>
+     
+
+      <div className="w-full min-h-screen text-white">
+        <div className="px-1 mx-auto max-w-6xl lg:-my-20 -my-10">
+          <div className="flex flex-col-reverse md:flex-row lg:items-end items-center">
+
+            {/* text */}
+            <motion.div className="pt-20 pb-0 px-3 xl:px-0 md:pl-4 text-center md:pb-12 md:w-1/2 md:text-left"
+              variants={leftVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <h1 id="header-text" className="relative z-10 text-4xl font-bold md:text-5xl font-display">
+                Code, Learn and Collaborate
+              </h1>
+              <p className="pt-8 text-lg leading-relaxed text-white md:max-w-md md:text-xl">
+                New way of collaborative programming. Share, communicate and
+                learn together
+              </p>
+              <div className="flex my-[25px] justify-center md:justify-start">
+                <Link href="/createroom">
+                  <a><h1 className="text-3xl">Get Started</h1></a>
+                </Link>
+                
+                <Link href="/">
+                  <button className="text-4xl mx-4 text-primary  animate-pulse">
+                    <BsArrowUpRightCircle/>
+                  </button>
+                </Link>
+
+              </div>
+              
+            </motion.div>
+
+            {/* illustration */}
+            <motion.div className="flex items-end md:w-1/2 mt-[10vh]"
+              variants={rightVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <Image src={Headerimg}  />
+            </motion.div>
+
+          </div>
+        </div>
+
+
+        <div className="flex justify-center mt-28 md:mt-52 mb-5 text-center">
+          <h3 className="text-4xl md:text-5xl font-medium">What is <span className="font-semibold italic">Code Together ?</span></h3>
+        </div>
+
+        {/* cards */}
+        <div className="xl:px-44 md:px-10 sm:px-28 p-10 md:pt-20 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+          {/* card 1 */}
+          <div className="rounded overflow-hidden px-4">
+            <Image src={CodeImg} />
+            <div className="py-4">
+              <div className="font-bold text-2xl mb-2">Collaborate Coding</div>
+              <p className="text-white text-xl mt-6">
+              Realtime code sharing among all the fellow programmers 
+              who are present in the room. Run code, share code and download code , get indulged in this wondeful environment.
+              </p>
+              <div id="explore-text" className="mt-5">
+              <Link href="/">
+                  <a className="cursor-pointer flex">
+                    <span className="text-xl mr-2">Explore now</span>
+                    <Image src={ArrowRightImg} />
+                  </a>
+
+                </Link>
+              </div>
+            </div>
+
+          </div>
+          {/* card 2 */}
+          <div className="rounded overflow-hidden px-4">
+            <Image src={CodeImg} />
+            <div className="py-4">
+              <div className="font-bold text-2xl mb-2">Seamless Communication</div>
+              <p className="text-white text-xl mt-6">
+              Realtime voice call, chat to appreciate seamless communication among the fellow programmers.
+              Smooth voice call to communicate , discuss codes through chat.
+              </p>
+              <div id="explore-text" className="mt-5">
+                <Link href="/">
+                  <a className="cursor-pointer flex">
+                    <span className="text-xl mr-2">Explore now</span>
+                    <Image src={ArrowRightImg} />
+                  </a>
+
+                </Link>
+              </div>
+            </div>
+
+          </div>
+
+          {/* card 3 */}
+          <div className="rounded overflow-hidden px-4">
+            <Image src={CodeImg} />
+            <div className="py-4">
+              <div className="font-bold text-2xl mb-2">Code assist 	&#38; Draft</div>
+              <p className="text-white text-xl mt-6">
+                Get help from our optimized search engine to find coding articles for you.
+              Discuss and plan the algorithmic approaches with others , do rough work through realtime whiteboard.
+              </p>
+              <div id="explore-text" className="mt-5">
+              <Link href="/">
+                  <a className="cursor-pointer flex">
+                    <span className="text-xl mr-2">Explore now</span>
+                    <Image src={ArrowRightImg} />
+                  </a>
+
+                </Link>
+              </div>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+        {/* editor image */}
+        <div className="mb-2 flex justify-center">
+          <Image src={CodeEditorImg} />
+        </div>
+
+     
+
+        {/* footer */}
+        <div className="pb-8 flex justify-center">
+          <span className="text-3xl">@ 2022 GrowerLabs</span>
+        </div>
+      </div>
+
     </>
-  )
+  );
 }
